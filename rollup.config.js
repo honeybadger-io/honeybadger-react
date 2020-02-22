6a525af9110cbd2ca0276051b65b8b76c093cd0c
Victor Bury <victor@c02wx0kag8wn.lan>
2020-02-22 13:42:02 +0100

    feat(update): babel and rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import resolve from '@rollup/plugin-node-resolve'
+import url from '@rollup/plugin-url'
 import svgr from '@svgr/rollup'
 import path from 'path'
 
@@ -54,7 +54,7 @@ export default {
     svgr(),
     babel({
       exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      plugins: [ '@babel/external-helpers' ]
     }),
     resolve(),
     commonjs()
