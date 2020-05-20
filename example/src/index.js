07811df55202e60fda4ab773815d6df05a8fd3a4
Joshua Wood <josh@joshuawood.net>
2020-05-20 10:11:16 -0700

    Update example

--- example/src/index.js
+++ example/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom'
 
 import './index.css'
 import App from './App'
+
 import Honeybadger from 'honeybadger-js'
 import ErrorBoundary from '@honeybadger-io/react'
 
-const config = {
-  api_key: (process.env.REACT_APP_HONEYBADGER_API_KEY || prompt('Enter the API key for your Honeybadger project:'))
-}
+Honeybadger.configure({
+  apiKey: (process.env.REACT_APP_HONEYBADGER_API_KEY || prompt('Enter the API key for your Honeybadger project:')),
+  environment: 'production'
+})
 
-const honeybadger = Honeybadger.configure(config)
-
-ReactDOM.render(<ErrorBoundary honeybadger={honeybadger}><App /></ErrorBoundary>, document.getElementById('root'))
+ReactDOM.render(
+  <ErrorBoundary honeybadger={Honeybadger}>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('root')
+)
